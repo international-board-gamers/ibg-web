58ac9010fc971f8997d8330f5f9a57548d4752b7
Derrick Eriksson <derrick.eriksson@gmail.com>
2021-06-27 22:16:13 +0200

    Added texts proposed by Lotta 🥇

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="home">
     <p>
-        You like board games no matter the form? 
-        You wish to meet new people while doing something fun?
-        Perhaps you want to discover new games?
-        Or you’re looking for players to try out that brand new game you just bought or to play this old one you love?
+      You like board games no matter the form? You wish to meet new people while doing something fun?
     </p>
 
     <p>
-        Whatever your reasons, welcome to International Board Gamers!
+      Then join us in International Board Gamers on our events! We are an association in Gothenburg and our purpose of the association is to meet new people and make friends!
     </p>
 
     <p>
-        This page is the meetup group for the International Board Gamers association in Gothenburg. 
-        The purpose of the association is to meet new people and make friends, especially with people from various corners of the world. 
-        We do this by organizing events related to board games and tabletop games.
+      The association organizes events related to boardgames so people can get new friends while playing boardgames. Any type of boardgame is welcomed. These events are free to join. Please join us on meetup.com for more infromation (link)
     </p>
 
     <p>
-        We organize gatherings once every week, alternatively on Fridays (18:00-22:00) and Sundays (15:30-20:00), to talk about board games in all their forms and of course to play the games we like!
-        Any type is welcome: from those that only take 10 minutes to those that last several hours, co-operative and competitive, cards, strategy, hidden-role games, from those that are played with only two players to those that gather a whole crowd!
-        These events are now free to join.
+      These events are free to join.
     </p>
 
     <p>
-        Note that the language of this group is English, both online and during our meetings, to ensure people from every horizon can participate.
+      In all our events we talk English to ensure all people from every part of the world feels welcomed
     </p>
 
-    <a href="https://www.meetup.com/international-board-gamers-gbg/">Join us on Meetup</a>
+    <a href="https://www.meetup.com/international-board-gamers-gbg/">Klick here to join us on Meetup straight away!</a>
   </div>
 </template>
 
--- src/views/Membership.vue
+++ src/views/Membership.vue
@@ -4,12 +4,16 @@
         <p>
             <ul>
                 <li>More opportunities to socialize</li>
-                <li>Access to IBG Slack community</li>
-                <li>Christmas dinner</li>
-                <li>Easter dinner or when exactly did we plan to do it?</li>
-                <li>IBG goes to-events (Escape room, that detective game etc)</li>
-                <li>Board games and BBQ outside in the warm part of the year, including while we have the IBG meetup summer break</li>
-                <li>Trading of games</li>
+                <li>Access to IBG Slack & Discord community</li>
+                <li>Access to IBG members-only events</li>
+                <li>Ability to influence the direction taken by IBG
+                    <ul>
+                        <li>elect board members</li>
+                        <li>vote for what IBG should do next</li>
+                        <li>suggest ideas</li>
+                        <li>get help and resources to implement your own ideas</li>
+                    </ul>
+                </li>
             </ul>
         </p>
         
@@ -17,19 +21,17 @@
         <p>
             Membership is a one-time cost for a full year which runs from 1st July to end of June next year
             <ul>
-                <li>350kr for full year</li>
-                <li>200kr from March 1st</li>
+                <li>200kr for full year</li>
             </ul>
         </p>
     </div>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
 div {
-    align: left;
+  align: left;
 }
 </style>

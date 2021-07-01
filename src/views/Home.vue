1b0333da4025ffc881372ed5a324e4dd82644502
Derrick Eriksson <DERIKS23@volvocars.com>
2021-07-01 11:54:40 +0200

    #13 Added text suggestions from Slack

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="home">
     <p>
-      You like board games no matter the form? You wish to meet new people while doing something fun?
+      Do you like board games no matter the form? Do you wish to meet new people while doing something fun?
     </p>
 
     <p>
-      Then join us in International Board Gamers on our events! We are an association in Gothenburg and our purpose of the association is to meet new people and make friends!
+      Then join us in International Board Gamers on our events! We are an association in Gothenburg whose purpose is to meet new people and make friends!
     </p>
 
     <p>
-      The association organizes events related to boardgames so people can get new friends while playing boardgames. Any type of boardgame is welcomed. These events are free to join. Please join us on meetup.com for more infromation (link)
+      The association organizes free weekly events which everybody can join to play board games with us and make some new friends. Please join us on <a href="https://www.meetup.com/international-board-gamers-gbg/">meetup.com</a> for more information
     </p>
 
     <p>
-      These events are free to join.
+      We talk English at our events to ensure all people from every part of the world feel welcome.
     </p>
 
     <p>
-      In all our events we talk English to ensure all people from every part of the world feels welcomed
+    <a href="https://www.meetup.com/international-board-gamers-gbg/">Click here to join us on Meetup straight away!</a>
     </p>
-
-    <a href="https://www.meetup.com/international-board-gamers-gbg/">Klick here to join us on Meetup straight away!</a>
   </div>
 </template>
 
